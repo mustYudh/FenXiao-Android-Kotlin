1bce52eb3356d2239d80137286ee7cf48e526000
yudenghao <denghao1019@163.com>
2019-06-30 17:28:21 +0800

    add:修改UI

--- app/src/main/java/com/nhbs/fenxiao/module/home/HomePageActivity.java
+++ app/src/main/java/com/nhbs/fenxiao/module/home/HomePageActivity.java
@@ -17,6 +17,7 @@ import com.nhbs.fenxiao.module.product.fragment.ProductFragment;
 import com.nhbs.fenxiao.module.store.MiniStoreFragment;
 import com.yu.common.mvp.PresenterLifeCycle;
 import com.yu.common.utils.PressHandle;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,35 +26,39 @@ import java.util.List;
  */
 public class HomePageActivity extends BaseActivity implements HomePageViewer {
 
-  private PressHandle pressHandle = new PressHandle(this);
+    private PressHandle pressHandle = new PressHandle(this);
 
-  @PresenterLifeCycle HomePagePresenter presenter = new HomePagePresenter(this);
-  private BottomNavigationView mBottomNavigationView;
+    @PresenterLifeCycle
+    HomePagePresenter presenter = new HomePagePresenter(this);
+    private BottomNavigationView mBottomNavigationView;
 
-  @Override protected void setView(@Nullable Bundle savedInstanceState) {
-    setContentView(R.layout.activity_home_page_view);
-  }
-
-  @Override protected void loadData() {
-    List<TabItem> items = new ArrayList<>();
-    mBottomNavigationView = bindView(R.id.navigation_view);
-    items.add(new TabView(0, new HomeFragment()));
-    items.add(new TabView(1, new ProductFragment()));
-    items.add(new TabView(2, null));
-    items.add(new TabView(3, new MiniStoreFragment()));
-    items.add(new TabView(4, new MineFragment()));
-    mBottomNavigationView.initControl(this).setPagerView(items, 0);
-    mBottomNavigationView.getControl().setOnTabClickListener((position, view) -> {
-      if (position == 2) {
-        HomeCenterPopUpWindow homePopUpWindow = new HomeCenterPopUpWindow(HomePageActivity.this);
-        homePopUpWindow.showPopupWindow();
-      }
-    });
-  }
-
-  @Override public void onBackPressed() {
-    if (!pressHandle.handlePress(KeyEvent.KEYCODE_BACK)) {
-      super.onBackPressed();
+    @Override
+    protected void setView(@Nullable Bundle savedInstanceState) {
+        setContentView(R.layout.activity_home_page_view);
     }
-  }
+
+    @Override
+    protected void loadData() {
+        List<TabItem> items = new ArrayList<>();
+        mBottomNavigationView = bindView(R.id.navigation_view);
+        items.add(new TabView(0, new HomeFragment()));
+        items.add(new TabView(1, new ProductFragment()));
+        items.add(new TabView(2, null));
+        items.add(new TabView(3, new MiniStoreFragment()));
+        items.add(new TabView(4, new MineFragment()));
+        mBottomNavigationView.initControl(this).setPagerView(items, 0);
+        mBottomNavigationView.getControl().setOnTabClickListener((position, view) -> {
+            if (position == 2) {
+                HomeCenterPopUpWindow homePopUpWindow = new HomeCenterPopUpWindow(HomePageActivity.this);
+                homePopUpWindow.showPopupWindow();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!pressHandle.handlePress(KeyEvent.KEYCODE_BACK)) {
+            super.onBackPressed();
+        }
+    }
 }
--- app/src/main/java/com/nhbs/fenxiao/module/store/MiniStoreFragment.java
+++ app/src/main/java/com/nhbs/fenxiao/module/store/MiniStoreFragment.java
@@ -3,31 +3,48 @@ package com.nhbs.fenxiao.module.store;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import com.nhbs.fenxiao.R;
-import com.nhbs.fenxiao.base.BaseFragment;
+import com.nhbs.fenxiao.base.BaseBarFragment;
 import com.nhbs.fenxiao.module.home.StatusBarColorManager;
 import com.yu.common.navigation.StatusBarFontColorUtil;
+import com.yu.common.ui.BadgeView;
 
 /**
  * @author yudenghao
  * @date 2019-06-04
  */
-public class MiniStoreFragment extends BaseFragment {
-  @Override protected int getContentViewId() {
-    return R.layout.fragment_mini_store_layout;
-  }
+public class MiniStoreFragment extends BaseBarFragment {
 
-  @Override protected void setView(@Nullable Bundle savedInstanceState) {
+    @Override
+    protected int getActionBarLayoutId() {
+        return R.layout.action_bar_page_fragment_mini_store_layout;
+    }
 
-  }
+    @Override
+    public boolean isImmersionBar() {
+        return true;
+    }
 
-  @Override protected void loadData() {
+    @Override
+    protected int getContentViewId() {
+        return R.layout.fragment_mini_store_layout;
+    }
 
-  }
+    @Override
+    protected void setView(@Nullable Bundle savedInstanceState) {
+        BadgeView badgeView = bindView(R.id.badge_view);
+        badgeView.setBadgeCount(1);
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
 
 
-  @Override protected void onPageInTop() {
-    super.onPageInTop();
-    StatusBarColorManager.INSTANCE.setDark(true);
-    StatusBarFontColorUtil.StatusBarLightMode(getActivity());
-  }
+    @Override
+    protected void onPageInTop() {
+        super.onPageInTop();
+        StatusBarColorManager.INSTANCE.setDark(false);
+        StatusBarFontColorUtil.statusBarDarkMode(getActivity());
+    }
 }
