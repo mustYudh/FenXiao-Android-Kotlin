03357f5cbcceb290bc6fa0f8a3635b121d5581fa
mks <keshuang.ma@easwift.cn>
2019-07-22 13:59:03 +0800

    commit

--- app/src/main/java/com/nhbs/fenxiao/module/store/fragment/MiniOpenStoreFragment.java
+++ app/src/main/java/com/nhbs/fenxiao/module/store/fragment/MiniOpenStoreFragment.java
@@ -2,6 +2,7 @@ package com.nhbs.fenxiao.module.store.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.nhbs.fenxiao.R;
 import com.nhbs.fenxiao.base.BaseBarFragment;
@@ -43,6 +44,7 @@ public class MiniOpenStoreFragment extends BaseBarFragment implements MiniOpenSt
         bindView(R.id.tv_open_store, view -> {
             //调转开店webview
             getLaunchHelper().startActivity(WebViewActivity.callIntent(getActivity(), "", "http://139.180.218.55:8080/openstore?token=" + UserProfile.getInstance().getAppToken()));
+            Log.e("token",UserProfile.getInstance().getAppToken());
         });
     }
 }
